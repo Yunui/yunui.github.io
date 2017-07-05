acd516a49b78896989ba93862ac0d95a5d049d9e
yunui <www@yunui.com>
2017-07-05 18:26:29 +0800

    Site updated: 2017-07-05 18:26:29

--- js/script.js
+++ js/script.js
@@ -88,8 +88,11 @@
 
         var bannerNode = $('.top-image');
         if(bannerNode.data('enable')){
-            var index = parseInt((Math.random() * 4) + 1);
+            var index = parseInt((Math.random() * 7) + 1);
             bannerNode.attr('style','background-image:url(/banner/'+index+'.jpg)');
+            bannerNode.attr('style','background-image:url(/banner/'+index+'.png)');
+            bannerNode.attr('style','background-image:url(/banner/'+index+'.jpeg)');
+
         }
     })
 
